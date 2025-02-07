f89c3b530e611a9224937033def08620efc2efb2
Angel Penchev <angel.penchev.tsb@gmail.com>
2025-02-07 18:20:27 +0200

    feat: add phone number to registered users

--- automobile/src/dev/penchev/automobile/entities/iam/RegisteredUser.java
+++ automobile/src/dev/penchev/automobile/entities/iam/RegisteredUser.java
@@ -9,16 +9,19 @@ public class RegisteredUser extends User {
     protected String email;
     protected String passwordHash;
 
+    protected String phoneNumber;
+
     protected List<Offer> listedOffers;
 
     protected LocalDateTime createdAt;
     protected LocalDateTime updatedAt;
 
-    public RegisteredUser(String email, String password, String firstName, String lastName) {
+    public RegisteredUser(String email, String phoneNumber, String password, String firstName, String lastName) {
         super(firstName, lastName, UserRole.USER);
 
         this.email = email;
         this.passwordHash = password;  // TODO: make passwords actual hashes
+        this.phoneNumber = phoneNumber;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
